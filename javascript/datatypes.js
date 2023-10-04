5105558a87fa25712eb06dd4a503a0e065dd5198
Ahidur Rahaman <100923020+bluekart123@users.noreply.github.com>
2023-10-04 07:54:26 +0000

    stack

--- javascript/datatypes.js
+++ javascript/datatypes.js
@@ -4,15 +4,36 @@
 // reference( non primitive)
 
 //Array,   Object,    Functions
-const artist = ["Arijit", "Lucky ali", "KK"] //Array
-const myObj = {
-    name: Ahidur,
-    Age: 23,
+//const artist = ["Arijit", "Lucky ali", "KK"] //Array
+// myObj = {
+    //name: Ahidur,
+    //Age: 23,
 // Object 
 
-}  
-console.log(myObj)
- 
+//}  
+//console.log(myObj)
+/* 
 const myFunction = function(){
     console.log("Hello World!") // this is functions
+}*/
+
+//memory Stack(Primitive), Heap(Non-Primitive)
+
+/* let myName= "Shakti"
+let anotherName = myName
+anotherName = "Ahidur"       => example of stack
+console.log(anotherName) 
+console.log(myName) */
+
+/* let userOne={
+    name: "Akash",
+    email:"ad@gmail.com"
 }
+let userTwo ={
+    name:"Soumit",
+    email: "Soumit@yahoo.com",
+}                                   => this is the example of heap 
+userTwo.email= "sd@gmail.com"
+userOne = userTwo
+console.log(userOne.email)
+*/
