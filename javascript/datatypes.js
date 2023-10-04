c090ceb73e8307231b5c3a989f4488a9a98d8059
Ahidur Rahaman <100923020+bluekart123@users.noreply.github.com>
2023-10-04 07:15:43 +0000

    done

---
+++ javascript/datatypes.js
@@ -0,0 +1,18 @@
+// {primitive datatypes
+// 7 types .... String, Number, boolean, null, undefined, Symbol. BigInt}
+
+// reference( non primitive)
+
+//Array,   Object,    Functions
+const artist = ["Arijit", "Lucky ali", "KK"] //Array
+const myObj = {
+    name: Ahidur,
+    Age: 23,
+// Object 
+
+}  
+console.log(myObj)
+ 
+const myFunction = function(){
+    console.log("Hello World!") // this is functions
+}
